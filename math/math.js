fb89d45fd3109b0fba152512c2c92f8de317e358
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-13 23:18:30 -0600

    formulas cuadrados y triangulos

---
+++ math/math.js
@@ -0,0 +1,28 @@
+
+const ladoCUadrado = 5
+const perimetroCUadrado = ladoCUadrado*4
+const areaCuadrado = ladoCUadrado*ladoCUadrado
+
+console.log({
+    ladoCUadrado,
+    perimetroCUadrado,
+    areaCuadrado
+});
+
+const ladoTriangulo1 = 6
+const ladoTriangulo2 = 6
+const ladoTrianguloBase = 4
+const alturaTriangulo = 5.5
+
+const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+ladoTrianguloBase
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2
+
+
+console.log({
+    ladoTriangulo1,
+    ladoTriangulo2,
+    ladoTrianguloBase,
+    alturaTriangulo,
+    perimetroTriangulo,
+    areaTriangulo
+});
