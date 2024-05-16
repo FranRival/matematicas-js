ec3d1633ee3702a55cc25a677262ce17fed33a01
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-15 21:39:55 -0600

    construyendo y modificando lo que doit con la function alv

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,15 @@
-function sacarPorcentaje(descuento, precio){
+const bton = document.querySelector('#calcular')
+const inoutPrice = document.querySelector('#price')
+bton.addEventListener('click', sacarPorcentaje)
+
+
+function sacarPorcentaje(descuento){
+    const precio = inoutPrice.value
 
     const resultado = precio * (100 - descuento)/100
-    return resultado
+    
+
+
+
 }
+
