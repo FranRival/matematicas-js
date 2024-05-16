0b347153778b79a52590a38dbd084ec0c183af57
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-15 21:58:04 -0600

    sacar el procentaje de algo bien

--- descuentos.js
+++ descuentos.js
@@ -1,15 +1,27 @@
 const bton = document.querySelector('#calcular')
 const inoutPrice = document.querySelector('#price')
+const inputDiscount = document.querySelector('#discount')
+const pResult = document.querySelector('#result')
 bton.addEventListener('click', sacarPorcentaje)
 
 
-function sacarPorcentaje(descuento){
-    const precio = inoutPrice.value
+function sacarPorcentaje(){
+    const precio = Number(inoutPrice.value)
+    const descuento = Number(inputDiscount.value)
+
+
+
+    if (!precio || !descuento) {
+        pResult.innerText('Llena el formulario alv')
+        return
+    }
+
+    if (descuento > 100) {
+        pResult.innerText('El descuento no puede ser del 100%')
+        return
+    }
 
     const resultado = precio * (100 - descuento)/100
-    
-
-
-
+    pResult.innerText = 'El nuevo precio con descuento es: $' + resultado
 }
 
