506eaa697491e626f0786507f915264594b5791e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-16 21:37:07 -0600

    esta madre fue transformada en un programa de descuento. but no podemos escribir en el apartado de escribe el descuento porque aun es una variable a llenar del tipo number

--- descuentos.js
+++ descuentos.js
@@ -1,25 +1,33 @@
 const bton = document.querySelector('#calcular')
 const inoutPrice = document.querySelector('#price')
-const inputDiscount = document.querySelector('#discount')
+const inputCoupon = document.querySelector('#cupon')
 const pResult = document.querySelector('#result')
 bton.addEventListener('click', sacarPorcentaje)
 
 
 function sacarPorcentaje(){
     const precio = Number(inoutPrice.value)
-    const descuento = Number(inputDiscount.value)
+    const cupon = inputCoupon.value
 
-
-
-    if (!precio || !descuento) {
+    if (!precio || !discuento) {
         pResult.innerText('Llena el formulario alv')
         return
     }
 
-    if (descuento > 100) {
-        pResult.innerText('El descuento no puede ser del 100%')
+    let discuento
+
+    if (cupon == 'Batman') {
+        discuento = 30
+    }else if(cupon =='descuentox2'){
+
+        discuento = 25
+    }else{
+        pResult.innerText('Cupon no valido')
         return
     }
+
+
+
 
     const resultado = precio * (100 - descuento)/100
     pResult.innerText = 'El nuevo precio con descuento es: $' + resultado
