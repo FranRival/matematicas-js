6f3ff038956240a54216ba396d4e4c92c9857fc4
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-18 21:59:44 -0600

    en que momento la variable lista de la funcion, se volvio un array y no una variable numerica?

--- math/promedio.js
+++ math/promedio.js
@@ -1,15 +1,15 @@
 function calcularPromedio(lista){
-    //sumar todos los elementos de l array y luego dividir con la cantidad de elementos.
 
-    //recorrer los elementos del array para sumarlos.
-
-    let sumaLista = 0
+/*     let sumaLista = 0
 
     for (let i = 0; i < lista.length; i++) {
         sumaLista = sumaLista + lista[i]
+    } */
 
-        //[1,2,3,4,5]
-    }
+
+    function sumarTodosElementos(){}
+
+    const sumaLista = lista.reduce()//metodo de funcion.
 
     const promedio = sumaLista / lista.length
     console.log(promedio);
@@ -17,6 +17,5 @@ function calcularPromedio(lista){
 
 }
 
-//para hacer funcionar this shit necesitas en la consola:
-
-//enviar una lista en formato array, con corchetes cuadrados.
+//metodo reduce.
+//en que dickenson la lista es un array y no una variable numerica o stringzoniana?
