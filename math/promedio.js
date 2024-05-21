a77e456f7bbf1248b047fdedabb79a8f93c6e275
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-20 22:52:20 -0600

    mismo codigo, resumido

--- math/promedio.js
+++ math/promedio.js
@@ -11,14 +11,19 @@ function calcularMediana(lista){
     const listaEsPar = esPar(lista)//es true o false
 
     if (listaEsPar) {
-        const indexMitad1ListaPar = (lista.length/2) - 1
-        const indexMitad2ListaPar = lista.length/2
+/*         const indexMitad1ListaPar = (lista.length/2) - 1
+        const indexMitad2ListaPar = lista.length/2 */
+
+        const Mitad1ListaPar = lista[(lista.length/2) - 1]
+        const Mitad2ListaPar = lista[lista.length/2]
 
         //indexMitad1ListaPar
         //--------| |-------------
         //indexMitad2ListaPar
         //estas 2 variables ubican el objetivo.
-        calcularPromedio([lista[indexMitad1ListaPar], lista[indexMitad2ListaPar]])
+
+        const listaMitades = [Mitad1ListaPar,Mitad2ListaPar]
+        calcularPromedio(listaMitades)
 
     }else{
         const indexMitadListaImpar = Math.floor(lista.length / 2)
